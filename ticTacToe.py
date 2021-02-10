3cfa97be4a3469b56a65280284b401c837611fb6
rakiyamsith <rakiyasmith@yahoo.com>
2021-02-10 14:17:50 -0800

    isWinner

--- ticTacToe.py
+++ ticTacToe.py
@@ -21,3 +21,10 @@ def isWinner(bo. le):
     # Given a board and a player's letter, this function returns True if th at player has won.
     # We use bo instead of board and le instead of letter so we don't have to type as much.
     return ((bo [7] == le and bo[8] == le and bo[9] == le) or # across the top)
+    (bo[4] == le and bo[5] == le and bo[6] == le) or # across the middle
+    (bo[1] == le and bo[2] == le and bo[3] == le) or # across the bottom
+    (bo[7] == le and bo[4] == le and bo[1] == le) or # down the left side
+    (bo[8] == le and bo[5] == le and bo[2] == le) or # down the middle 
+    (bo[9] == le and bo[6] == le and bo[3] == le) or # down the right side 
+    (bo[7] == le and bo[5] == le and bo[3] == le) or # diagonal
+    (bo[9] == le and bo[5] == ;e and bo[1] == le) or # diagonal
