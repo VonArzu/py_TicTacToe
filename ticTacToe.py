a403e4d4d9794284ece0e34648afbd4119e2e4bc
VonArzu <Von.Arzu@rutgers.edu>
2021-02-10 17:58:40 -0500

    Erroe still there

--- ticTacToe.py
+++ ticTacToe.py
@@ -28,7 +28,8 @@ def isWinner(bo, le) :
     (bo[9] == le and bo[6] == le and bo[3] == le) or # down the right side 
     (bo[7] == le and bo[5] == le and bo[3] == le) or # diagonal
     (bo[9] == le and bo[5] == le and bo[1] == le) or # diagonal
-def main():
+
+def main() :
     #Main game loop
     print('Welcome to Tic Tac Toe, to win complete a straight line of your letter (Diagonal, Horizontal, Vertical). The board has positions 1-9 starting at the top left.')
     printBoard()
