2ab94423889a755b616abbbc9e19418730e053e1
VonArzu <Von.Arzu@rutgers.edu>
2021-02-10 18:17:30 -0500

    Redid Tic Tac Toe

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,90 +1,52 @@
-board = [' ' for x in range (10)] # this should be the first line in the program
-# board is now: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',]
+#Tic Tac Toe game in python
+
+board = [' ' for x in range(10)]
+
 def insertLetter(letter, pos):
     board[pos] = letter
-def spaceIsFree(pos) :
-    return board [pos] == ' '
-def printBoard(board0):
-    #"board" is a list of 10 strings representing the board (ingore index 0)
-    print('    |    | ')
-    print('  ' + board [1]  +  '  |  '  + board [2]  + '  |   '  +  board[3])
-    print('    |   |')
+
+def spaceIsFree(pos):
+    return board[pos] == ' '
+
+def printBoard(board):
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
     print('-----------')
-    print('    |   |')
-    print('  ' + board [4]  +  '  |  '  +  board[5]  + '  |   '  +  board [6])
-    print('    |   |')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
     print('-----------')
-    print('    |   |')
-    print('  ' + board[7]   +   '  |  '  + board[8]  +  ' |  '   + board[9])
-    print('    |   |')
-def isWinner(bo, le) :
-    # Given a board and a player's letter, this function returns True if th at player has won.
-    # We use bo instead of board and le instead of letter so we don't have to type as much.
-    return ((bo [7] == le and bo[8] == le and bo[9] == le) or # across the top)
-    (bo[4] == le and bo[5] == le and bo[6] == le) or # across the middle
-    (bo[1] == le and bo[2] == le and bo[3] == le) or # across the bottom
-    (bo[7] == le and bo[4] == le and bo[1] == le) or # down the left side
-    (bo[8] == le and bo[5] == le and bo[2] == le) or # down the middle 
-    (bo[9] == le and bo[6] == le and bo[3] == le) or # down the right side 
-    (bo[7] == le and bo[5] == le and bo[3] == le) or # diagonal
-    (bo[9] == le and bo[5] == le and bo[1] == le) or # diagonal
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    
+def isWinner(bo, le):
+    return (bo[7] == le and bo[8] == le and bo[9] == le) or (bo[4] == le and bo[5] == le and bo[6] == le) or(bo[1] == le and bo[2] == le and bo[3] == le) or(bo[1] == le and bo[4] == le and bo[7] == le) or(bo[2] == le and bo[5] == le and bo[8] == le) or(bo[3] == le and bo[6] == le and bo[9] == le) or(bo[1] == le and bo[5] == le and bo[9] == le) or(bo[3] == le and bo[5] == le and bo[7] == le)
 
-def main() :
-    #Main game loop
-    print('Welcome to Tic Tac Toe, to win complete a straight line of your letter (Diagonal, Horizontal, Vertical). The board has positions 1-9 starting at the top left.')
-    printBoard()
-
-    while not(isBoardFull(board)):
-        if not(isWinner(board, 'O')):
-            playerMove()
-            printBoard()
-        else:
-            print('O\'s win this time...')
-            break
-
-        
-        if not(isWinner(board, 'X')):
-            move = compMove()
-            if move == 0:
-                print('Game is a Tie! No more spaces left to move.')
-            else:
-                insertBoard('O', move)
-                print('Computer placed an \'O\' in position', move, ':')
-                printBoard()
-        else:
-            print('X\'s win, good job!')
-            break
-
-
-    if isBoardFull(board):
-        print('Game is a tie! No more spaces left to move.')
-        def isBoardFull(board):
-    if board.count(' ') > 1:  # Since we always have one blank element in board we must use > 1
-        return False
-    else:
-        return True
-        def playerMove():
+def playerMove():
     run = True
-    while run:  # Keep looping until we get a valid move
+    while run:
         move = input('Please select a position to place an \'X\' (1-9): ')
         try:
-            move  = int(move)
-            if move > 0 and move < 10:  # makes sure we type in a number between 1-9
-                if spaceIsFree(move):  # check if the move we choose is valid (no other letter is there already)
+            move = int(move)
+            if move > 0 and move < 10:
+                if spaceIsFree(move):
                     run = False
-                    insertBoard('X', move)
+                    insertLetter('X', move)
                 else:
-                    print('This postion is already occupied!')
+                    print('Sorry, this space is occupied!')
             else:
                 print('Please type a number within the range!')
         except:
             print('Please type a number!')
-            def compMove():
-    possibleMoves = [x for x, letter in enumerate(board) if letter == ' ' and x != 0] # Create a list of possible moves
+            
+
+def compMove():
+    possibleMoves = [x for x, letter in enumerate(board) if letter == ' ' and x != 0]
     move = 0
-    
-    #Check for possible winning move to take or to block opponents winning move
-    for let in ['O','X']:
+
+    for let in ['O', 'X']:
         for i in possibleMoves:
             boardCopy = board[:]
             boardCopy[i] = let
@@ -92,38 +54,70 @@ def main() :
                 move = i
                 return move
 
-
-    #Try to take one of the corners
     cornersOpen = []
     for i in possibleMoves:
         if i in [1,3,7,9]:
             cornersOpen.append(i)
+            
     if len(cornersOpen) > 0:
         move = selectRandom(cornersOpen)
         return move
-    
-    #Try to take the center
+
     if 5 in possibleMoves:
         move = 5
         return move
 
-    #Take any edge
     edgesOpen = []
     for i in possibleMoves:
         if i in [2,4,6,8]:
             edgesOpen.append(i)
-    
+            
     if len(edgesOpen) > 0:
         move = selectRandom(edgesOpen)
-
+        
     return move
-    def selectRandom(li):
+
+def selectRandom(li):
     import random
     ln = len(li)
-    r = random.randrange(0, ln)
+    r = random.randrange(0,ln)
     return li[r]
+    
 
-    while True:
+def isBoardFull(board):
+    if board.count(' ') > 1:
+        return False
+    else:
+        return True
+
+def main():
+    print('Welcome to Tic Tac Toe!')
+    printBoard(board)
+
+    while not(isBoardFull(board)):
+        if not(isWinner(board, 'O')):
+            playerMove()
+            printBoard(board)
+        else:
+            print('Sorry, O\'s won this time!')
+            break
+
+        if not(isWinner(board, 'X')):
+            move = compMove()
+            if move == 0:
+                print('Tie Game!')
+            else:
+                insertLetter('O', move)
+                print('Computer placed an \'O\' in position', move , ':')
+                printBoard(board)
+        else:
+            print('X\'s won this time! Good Job!')
+            break
+
+    if isBoardFull(board):
+        print('Tie Game!')
+
+while True:
     answer = input('Do you want to play again? (Y/N)')
     if answer.lower() == 'y' or answer.lower == 'yes':
         board = [' ' for x in range(10)]
@@ -131,4 +125,3 @@ def main() :
         main()
     else:
         break
-
